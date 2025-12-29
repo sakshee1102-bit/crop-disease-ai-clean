35ccebbc81cf89f37435e5e1e9eeb4d38dff7e6d
Shubham Jare <smjare07@gmail.com>
2025-12-29 22:04:08 +0530

    Final working demo code

--- app/app.py
+++ app/app.py
@@ -16,11 +16,9 @@ UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -38,7 +36,6 @@ def predict():
     result = predict_disease(path)
     return jsonify(result)
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
